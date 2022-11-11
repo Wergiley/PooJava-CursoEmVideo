9e3d6bb9692f599d3af915c0b47e3f0f90edbff7
Wergiley <werslley.robson@gmail.com>
2022-11-11 20:58:13 -0300

    att

--- ExerciciosJavaObjetos/Ex02/Aula02.java
+++ ExerciciosJavaObjetos/Ex02/Aula02.java
@@ -4,8 +4,9 @@ package ExerciciosJavaObjetos.Ex02;
 public class Aula02 {
     
     public static void main(String[] args) {
+        // Objeto c1 criado...
         Caneta c1 = new Caneta();
-
+        // Atributos do Objeto.
         c1.cor = "Azul";
         c1.ponta = 1.5f;
         c1.modelo = "Bic";
@@ -16,5 +17,22 @@ public class Aula02 {
 
         c1.status();
         c1.rabiscar();
+
+        // Novo Objeto c2 criado...
+
+        Caneta c2 = new Caneta();
+        //Atributos
+        c2.cor = "Preta";
+        c2.ponta = 0.5f;
+        c2.modelo = "Pen";
+        c2.carga = 90;
+        c2.tampada = true;
+
+        // Chamada dos metodos
+        c2.status();
+        c2.rabiscar();
+
+        // Final da aula, crie um novo objeto semelhante, com dois objetos fisicos, e dois objetos abstratos que você tenha proximo de você.
+        // conclua a aula a termina os 4 Objetos criados.
     }
 }
--- ExerciciosJavaObjetos/Ex02/Caneta.java
+++ ExerciciosJavaObjetos/Ex02/Caneta.java
@@ -1,5 +1,5 @@
 package ExerciciosJavaObjetos.Ex02;
-
+// Atributos da Classe Caneta.
 public class Caneta {
     String modelo;
     String cor;
@@ -7,8 +7,9 @@ public class Caneta {
     int carga;
     boolean tampada;
 
+      //metodos da caneta...
     void status(){
-        System.out.println("Informações da caneta:");
+        System.out.println("\nInformações da caneta:");
         System.out.println("Cor: "+this.cor);
         System.out.println("Modelo: "+this.modelo);
         System.out.println("Ponta: "+this.ponta);
@@ -17,18 +18,18 @@ public class Caneta {
     }
 
     void rabiscar() {
-        if(tampada==true) {
+        if(tampada==false) {
             System.out.println("Estou rabiscando!.");
         } else {
-            System.out.println("Não posso rabicar!");
+            System.out.println("Não posso rabiscar!");
         }
     }
 
     void tampar() {
-        
+       System.out.println("Está tampada");
     }
 
     void destampar() {
-        
+        System.out.println("Não está tampada");
     }
 }
